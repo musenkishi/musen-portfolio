56b4dc11954f7a11a7dc4392fa1c03acb595a93c
Freddie Lust-Hed <musenkishi@gmail.com>
2025-02-23 10:25:10 +0100

    add view source link

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,15 +2,25 @@ import { Button } from "@/components/components/ui/button"
 import { FC } from "react"
 import Logo from "./logo"
 import { PageLink } from "./link/page-link"
+import SocialLink from "./link/social-link"
+import { RiGithubFill } from "react-icons/ri"
 
 const NavBar: FC = () => {
   return (
     <nav className="fixed bg-gray-800/50 backdrop-blur-sm z-10 w-full flex items-center justify-center">
-      <div className="p-4 relative flex items-center justify-start h-14 w-full lg:max-w-1/2 ">
-        <Logo />
-        <Button asChild variant="link">
-          <PageLink href="/projects">Projects</PageLink>
-        </Button>
+      <div className="p-4 relative flex items-center justify-between h-14 w-full lg:max-w-240">
+        <div className="flex items-center justify-between">
+          <Logo />
+          <Button asChild variant="link">
+            <PageLink href="/projects">Projects</PageLink>
+          </Button>
+        </div>
+          <SocialLink
+            href="https://github.com/musenkishi/musenkishi-homepage"
+            icon={<RiGithubFill />}
+            label="View source"
+            hideLabelOnSmall
+          />
       </div>
     </nav>
   )
